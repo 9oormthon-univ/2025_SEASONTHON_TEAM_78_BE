f5595130eb6f7164cc233cc14b8a06fbf351359c
taehoondev030 <taehoondev030@gmail.com>
2025-09-04 23:54:36 +0900

    docs: 인증글 API 요청 dto 예시 데이터 추가

--- src/main/java/com/minimo/backend/certification/dto/request/CreateCertificationRequest.java
+++ src/main/java/com/minimo/backend/certification/dto/request/CreateCertificationRequest.java
@@ -1,5 +1,6 @@
 package com.minimo.backend.certification.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,9 +14,11 @@ public class CreateCertificationRequest {
 
     @NotBlank
     @Size(max=20)
+    @Schema(description = "인증 게시물 제목", example = "1일차 인증!")
     private String title;
 
     @NotBlank
     @Size(max=300)
+    @Schema(description = "인증 게시물 내용", example = "오늘은 아침에 기상하자마자 물을 1L 마셨습니다!")
     private String content;
 }
--- src/main/java/com/minimo/backend/certification/dto/request/UpdateCertificationRequest.java
+++ src/main/java/com/minimo/backend/certification/dto/request/UpdateCertificationRequest.java
@@ -1,5 +1,6 @@
 package com.minimo.backend.certification.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,9 +12,11 @@ public class UpdateCertificationRequest {
 
     @NotBlank
     @Size(max=20)
+    @Schema(description = "변경할 챌린지 제목", example = "DAY-1 오늘의 인증")
     private String title;
 
     @NotBlank
     @Size(max=300)
+    @Schema(description = "변경할 챌린지 내용", example = "오늘은 아침 러닝 5km를 실시했습니다!")
     private String content;
 }

3f70eed0bf0e75fefda8555ef2e8f90ec27e0d8e
taehoondev030 <taehoondev030@gmail.com>
2025-09-04 20:40:28 +0900

    docs: 챌린지 관련 API 요청 dto 예시 데이터 추가

--- src/main/java/com/minimo/backend/challenge/dto/request/CreateChallengeRequest.java
+++ src/main/java/com/minimo/backend/challenge/dto/request/CreateChallengeRequest.java
@@ -1,5 +1,6 @@
 package com.minimo.backend.challenge.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,13 +12,17 @@ public class CreateChallengeRequest {
 
     @NotBlank
     @Size(max=20)
+    @Schema(description = "챌린지 제목", example = "하마가 될래요")
     private String title;
 
     @NotBlank
     @Size(max=50)
+    @Schema(description = "챌린지 내용", example = "하루에 물 2L 마시기")
     private String content;
 
+    @Schema(description = "챌린지 진행 기간", example = "7")
     private int durationDays;
 
+    @Schema(description = "챌린지 아이콘", example = "pencil")
     private String challengeIcon;
 }
--- src/main/java/com/minimo/backend/challenge/dto/request/FindChallengeRequest.java
+++ src/main/java/com/minimo/backend/challenge/dto/request/FindChallengeRequest.java
@@ -1,6 +1,7 @@
 package com.minimo.backend.challenge.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ public class FindChallengeRequest {
 
     @NotNull(message = "조회할 날짜는 필수입니다.")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @Schema(description = "챌린지 목록을 조회할 날짜", example = "2025-08-25")
     private LocalDate date;
 }

b6eb360ed9b3910eb7f422899c31bee35911fc72
AYEOOON <k93853471@gmail.com>
2025-09-06 20:53:25 +0900

    refactor: CertificationRepositoryCustom 페이지네이션 반환 정의
    - List → GlobalPageResponse<CertificationFeedResponse> 반환 적용

--- src/main/java/com/minimo/backend/certification/repository/CertificationRepositoryCustom.java
+++ src/main/java/com/minimo/backend/certification/repository/CertificationRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.minimo.backend.certification.repository;
 
 import com.minimo.backend.certification.dto.response.CertificationFeedResponse;
+import com.minimo.backend.global.response.GlobalPageResponse;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CertificationRepositoryCustom {
-    List<CertificationFeedResponse> findCertificationFeed(Long userId, Pageable pageable);
+    GlobalPageResponse<CertificationFeedResponse> findCertificationFeed(Long userId, Pageable pageable);
 }
